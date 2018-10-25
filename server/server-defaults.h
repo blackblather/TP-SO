5031b43fbca434787f489d0dd74a7cab48421744
Joao <joao.monteiro95@hotmail.com>
2018-10-25 16:59:35 +0100

    Defined default main named pipe name and created structs

--- server/server-defaults.h
+++ server/server-defaults.h
@@ -1,6 +1,14 @@
-#define MEDIT_MAXLINES 15						//Apenas usar se a envVar nao estiver definida (same name)
-#define MEDIT_MAXCOLUMNS 45						//Apenas usar se a envVar nao estiver definida (same name)
-#define MEDIT_DB_NAME "medit.db"				//Nome por omissao da base de dados
-#define MEDIT_NR_OF_INTERACTION_NAMED_PIPES 5	//Valor aleatório (não encontrei um no enunciado)
-#define MEDIT_TIMEOUT 10						//Valor em segundos. Possivel de substituir por uma envVar (with the same name)
-#define MEDIT_MAXUSERS 3						//Usada caso a envVar nao esteja definida
+#define MEDIT_MAXLINES 15							//Apenas usar se a envVar nao estiver definida (same name)
+#define MEDIT_MAXCOLUMNS 45							//Apenas usar se a envVar nao estiver definida (same name)
+#define MEDIT_DB_NAME "medit.db"					//Nome por omissao da base de dados
+#define MEDIT_NR_OF_INTERACTION_NAMED_PIPES 5		//Valor aleatório (não encontrei um no enunciado)
+#define MEDIT_MAIN_NAMED_PIPE_NAME "mainNamedPipe"	//Nome do named pipe pricipal
+#define MEDIT_TIMEOUT 10							//Valor em segundos. Possivel de substituir por uma envVar (with the same name)
+#define MEDIT_MAXUSERS 3							//Usada caso a envVar nao esteja definida
+typedef struct LineStruct{
+	char *column;
+	char *username;
+} Line;
+typedef struct ScreenStruct{
+	Line *line;
+} Screen;
