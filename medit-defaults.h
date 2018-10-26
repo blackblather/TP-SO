c604783e95f6a13f8ec3b3d8240fff25e281aea9
Joao <joao.monteiro95@hotmail.com>
2018-10-26 21:20:25 +0100

    Moved MEDIT_MAXLINES, MEDIT_MAXCOLUMNS AND MEDIT_MAXUSERS to server-defaults.h

--- medit-defaults.h
+++ medit-defaults.h
@@ -1,8 +1,5 @@
 //Constantes comuns ao servidor e ao cliente
 #define MEDIT_USERNAME_MAXLENGHT 8
-#define MEDIT_MAXLINES 15							//Apenas usar se a envVar nao estiver definida (same name)
-#define MEDIT_MAXCOLUMNS 45							//Apenas usar se a envVar nao estiver definida (same name)
-#define MEDIT_MAIN_NAMED_PIPE_NAME "mainNamedPipe"	//Nome do named pipe pricipal
 typedef struct CommonSettingsStruct{
 	int maxLines, maxColumns;
 	char *mainNamedPipeName;
