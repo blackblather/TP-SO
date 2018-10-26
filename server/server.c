878590d6c14ae24c705995e1ac537eee4eaf33f6
Joao <joao.monteiro95@hotmail.com>
2018-10-26 04:11:47 +0100

    Finished InitFromOpts() and reorganized the code to use ServerSettingsStruct

--- server/server.c
+++ server/server.c
@@ -3,8 +3,8 @@
 #include <errno.h>	//Used for error handling in strtol()
 #include <stdlib.h>	//Used for strtol()
 #include <unistd.h>	//Used for access() and getopt()
-#include "server-defaults.h"
 #include "../medit-defaults.h"
+#include "server-defaults.h"
 void FlushStdin(){
 	//Source: https://stackoverflow.com/questions/2187474/i-am-not-able-to-flush-stdin
 	//"Remove" os chars do buffer até chegar ao '\n' ou End Of File (EOF)
@@ -55,7 +55,7 @@ void FreeLine(char* nrStr, int maxLines){
     	printf("Invalid line.\n");
 }
 
-void Settings(int maxLines, int maxColumns, char* dbFilename, int maxUsers, int nrOfInteractionNamedPipes, char* mainNamedPipeName){
+void PrintSettings(int maxLines, int maxColumns, char* dbFilename, int maxUsers, int nrOfInteractionNamedPipes, char* mainNamedPipeName){
 	printf("Max lines: %d\n", maxLines);
 	printf("Max columns: %d\n", maxColumns);
 	printf("Database filename: %s\n", dbFilename);
@@ -82,25 +82,21 @@ int UserExists(char *filename, char *username){
 	return 0;
 }
 
-void ProcessCommand(char* cmd, int* shutdown, int maxLines, int maxColumns, char* dbFilename){
-	char *cmdPart1, *cmdPart2,
-		 *mainNamedPipeName = MEDIT_MAIN_NAMED_PIPE_NAME;
-
-	int maxUsers = MEDIT_MAXUSERS,
-		nrOfInteractionNamedPipes = MEDIT_NR_OF_INTERACTION_NAMED_PIPES;
+void ProcessCommand(char* cmd, int* shutdown, CommonSettings commonSettings, ServerSettings serverSettings){
+	char *cmdPart1, *cmdPart2;
 
 	if((cmdPart1 = strtok(cmd, " ")) != NULL){
 		if(strcmp(cmdPart1, "load")==0 || strcmp(cmdPart1, "save")==0 || strcmp(cmdPart1, "free")==0){
 			if((cmdPart2 = strtok(NULL, " ")) != NULL){
 				if(strcmp(cmdPart1, "load")==0) LoadFile(cmdPart2);
 				else if(strcmp(cmdPart1, "save")==0) SaveFile(cmdPart2);
-				else if(strcmp(cmdPart1, "free")==0) FreeLine(cmdPart2, maxLines);
+				else if(strcmp(cmdPart1, "free")==0) FreeLine(cmdPart2, commonSettings.maxLines);
 			}
 			else
 				printf("Missing second parameter.\n");
 		}
 		else{
-			if(strcmp(cmdPart1, "settings")==0) Settings(maxLines, maxColumns, dbFilename, maxUsers, nrOfInteractionNamedPipes, mainNamedPipeName);
+			if(strcmp(cmdPart1, "settings")==0) PrintSettings(commonSettings.maxLines, commonSettings.maxColumns, serverSettings.dbFilename, serverSettings.maxUsers, serverSettings.nrOfInteractionNamedPipes, commonSettings.mainNamedPipeName);
 			else if(strcmp(cmdPart1, "userExists")==0) {
 				if(UserExists("medit.db", "user1"))
 					printf("Encontrou o utilizador\n");
@@ -125,28 +121,34 @@ void AllocScreenMemory(Screen *screen, int maxLines, int maxColumns){
 	}
 }
 
-void InitFromOpts(int argc, char* const argv[], char *dbFilename){
+void UpdateDbFilename(char **dbFilename, char *newDbFilename){
+	if(FileExists(newDbFilename))
+		(*dbFilename) = newDbFilename;
+}
+
+void UpdateMainNamedPipeName(char **oldMainNamedPipeName, char *newdMainNamedPipeName){
+	(*oldMainNamedPipeName) = newdMainNamedPipeName;
+}
+
+void UpdateNumberOfInteractiveNamedPipes(int* oldNrOfInteractionNamedPipes, char *newNrOfInteractionNamedPipesSTR){
+	char* end;
+	int nrOfInteractionNamedPipesTMP = (int)strtol(newNrOfInteractionNamedPipesSTR, &end, 10);
+	if (errno == ERANGE || end == newNrOfInteractionNamedPipesSTR){
+		nrOfInteractionNamedPipesTMP = -1;
+        errno = 0;
+    }
+    if(nrOfInteractionNamedPipesTMP > 0)
+    	(*oldNrOfInteractionNamedPipes) = nrOfInteractionNamedPipesTMP;
+}
+
+void InitFromOpts(int argc, char* const argv[], char **dbFilename, char **mainNamedPipeName, int* nrOfInteractionNamedPipes){
 	//Source: https://www.gnu.org/software/libc/manual/html_node/Getopt.html
 	int c;
 	while ((c = getopt (argc, argv, "f:p:n:")) != -1){
 	    switch (c){
-	      case 'f': break;
-	      case 'p':
-	        //cvalue = optarg;
-	        break;
-	      case 'n': break;
-	      case '?':
-	        /*if (optopt == 'c')
-	          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-	        else if (isprint (optopt))
-	          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-	        else
-	          fprintf (stderr,
-	                   "Unknown option character `\\x%x'.\n",
-	                   optopt);*/
-	        break;
-	      default:
-	        abort ();
+	      case 'f': UpdateDbFilename(dbFilename, optarg); break;
+	      case 'p':	UpdateMainNamedPipeName(mainNamedPipeName, optarg); break;
+	      case 'n': UpdateNumberOfInteractiveNamedPipes(nrOfInteractionNamedPipes, optarg); break;
 	    }
 	}
 }
@@ -160,30 +162,47 @@ void FreeAllocatedMemory(Screen *screen, int maxLines, int maxColumns){
 	for(int i = 0; i < maxLines; i++)
 		free((*screen).line[i].column);
 	free((*screen).line);
-	//free(OccupiedLine);
+}
+
+void InitCommonSettingsStruct(CommonSettings* commonSettings){
+	(*commonSettings).maxLines = MEDIT_MAXLINES;
+	(*commonSettings).maxColumns = MEDIT_MAXCOLUMNS;
+	(*commonSettings).mainNamedPipeName = MEDIT_MAIN_NAMED_PIPE_NAME;
+}
+
+void InitServerSettingsStruct(ServerSettings* serverSettings){
+	(*serverSettings).nrOfInteractionNamedPipes = MEDIT_NR_OF_INTERACTION_NAMED_PIPES;
+	(*serverSettings).timeout = MEDIT_TIMEOUT;
+	(*serverSettings).maxUsers = MEDIT_MAXUSERS;
+	(*serverSettings).dbFilename = MEDIT_DB_NAME;
+}
+
+void InitSettingsStructs(CommonSettings* commonSettings, ServerSettings* serverSettings){
+	InitCommonSettingsStruct(commonSettings);
+	InitServerSettingsStruct(serverSettings);
 }
 
 int main(int argc, char* const argv[]){
-	//scanf(" %14[^ \n]%*[^ \n]%*[ \n]%255[^\n]",cmdPart1, cmdPart2);
-	//printf("CMD1->%s\nCMD2->%s\n", cmdPart1, cmdPart2);
-	
-	char cmd[300] = {'\0'},
-		 *dbFilename = MEDIT_DB_NAME;
-	int maxLines = MEDIT_MAXLINES,
-		maxColumns = MEDIT_MAXCOLUMNS;
-	Screen screen;		//Exemplo de utilização: screen.line[0].column[0] = 'F';
-	Line *OccupiedLine;	//Linha(s) em edição
-
-	InitFromOpts(argc, argv, dbFilename);
-	InitFromEnvs(&screen, &maxLines, &maxColumns);
+	char cmd[300] = {'\0'};
 	int shutdown = 0;
+
+	CommonSettings commonSettings;
+	ServerSettings serverSettings;
+	Screen screen;					//Exemplo de utilização: screen.line[0].column[0] = 'F';
+	Line *occupiedLine;				//Linha(s) em edição	(UNUSED AT THE MOMENT)
+
+	InitSettingsStructs(&commonSettings, &serverSettings);
+	InitFromOpts(argc, argv, &serverSettings.dbFilename, &commonSettings.mainNamedPipeName, &serverSettings.nrOfInteractionNamedPipes);
+	InitFromEnvs(&screen, &commonSettings.maxLines, &commonSettings.maxColumns);
+
 	do{
 		printf("Command: ");
 		scanf(" %299[^\n]", cmd);
 		FlushStdin();
-		ProcessCommand(cmd, &shutdown, maxLines, maxColumns, dbFilename);
+		ProcessCommand(cmd, &shutdown, commonSettings, serverSettings);
 	}while(shutdown == 0);
+
 	//DO SHUTDOWN LOGIC HERE
-	FreeAllocatedMemory(&screen, maxLines, maxColumns);
+	FreeAllocatedMemory(&screen, commonSettings.maxLines, commonSettings.maxColumns);
 	return 0;
 }
