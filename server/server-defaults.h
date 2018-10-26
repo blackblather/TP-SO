574c94d06adaa83b870d61bf29edefa2dd17dc0a
Joao <joao.monteiro95@hotmail.com>
2018-10-26 04:09:47 +0100

    Moved common consts outside of this file, and created ServerSettingsStruct

--- server/server-defaults.h
+++ server/server-defaults.h
@@ -1,10 +1,8 @@
-#define MEDIT_MAXLINES 15							//Apenas usar se a envVar nao estiver definida (same name)
-#define MEDIT_MAXCOLUMNS 45							//Apenas usar se a envVar nao estiver definida (same name)
-#define MEDIT_DB_NAME "medit.db"					//Nome por omissao da base de dados
 #define MEDIT_NR_OF_INTERACTION_NAMED_PIPES 5		//Valor aleatório (não encontrei um no enunciado)
-#define MEDIT_MAIN_NAMED_PIPE_NAME "mainNamedPipe"	//Nome do named pipe pricipal
 #define MEDIT_TIMEOUT 10							//Valor em segundos. Possivel de substituir por uma envVar (with the same name)
 #define MEDIT_MAXUSERS 3							//Usada caso a envVar nao esteja definida
+#define MEDIT_DB_NAME "medit.db"					//Nome por omissao da base de dados
+
 typedef struct LineStruct{
 	char *column;
 	char *username;
@@ -12,3 +10,9 @@ typedef struct LineStruct{
 typedef struct ScreenStruct{
 	Line *line;
 } Screen;
+typedef struct ServerSettingsStruct{
+	int nrOfInteractionNamedPipes,
+		timeout,
+		maxUsers;
+	char *dbFilename;
+} ServerSettings;
