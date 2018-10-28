92b490f3f95a174c9fc3a03d965e871ec8010eab
Joao <joao.monteiro95@hotmail.com>
2018-10-28 01:38:16 +0000

    Added 'int lineNumber' to LineStruct

--- server/server-defaults.h
+++ server/server-defaults.h
@@ -3,12 +3,13 @@
 #define MEDIT_TIMEOUT 10							//Valor em segundos. Possivel de substituir por uma envVar (with the same name)
 #define MEDIT_MAXUSERS 3							//Usada caso a envVar nao esteja definida
 #define MEDIT_DB_NAME "medit.db"					//Nome por omissao da base de dados
-//Defines usados oara inicalizar CommonSettingsStruct
+//Defines usados para inicalizar CommonSettingsStruct
 #define MEDIT_MAXLINES 15							//Apenas usar se a envVar nao estiver definida (same name)
 #define MEDIT_MAXCOLUMNS 45							//Apenas usar se a envVar nao estiver definida (same name)
 #define MEDIT_MAIN_NAMED_PIPE_NAME "mainNamedPipe"	//Nome do named pipe pricipal
 
 typedef struct LineStruct{
+	int lineNumber;		//Used to know current line (occupiedLine)
 	char *column;
 	char *username;
 } Line;
