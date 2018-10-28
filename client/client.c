f2bbbc30763a8f1d0868c88e03b82ad8fca9af2a
Joao <joao.monteiro95@hotmail.com>
2018-10-28 01:36:10 +0000

    CLIENT META 1 FINITO BABYYYYYY

--- client/client.c
+++ client/client.c
@@ -1,14 +1,16 @@
-#include <stdio.h>	//Used for scanf() and stuff
-#include <string.h>	//Used for strlen()
+#include <stdio.h>
+#include <ncurses.h>
+#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>	//Used for access() and getopt()
+#include <unistd.h>
 #include "client-defaults.h"
 
-void FlushStdin(){
-	//Source: https://stackoverflow.com/questions/2187474/i-am-not-able-to-flush-stdin
-	//"Remove" os chars do buffer até chegar ao '\n' ou End Of File (EOF)
-	int f;
-	while((f = fgetc(stdin)) != '\n' && f != EOF);
+int IsValidChar(int ch){
+	//Source: https://theasciicode.com.ar/ascii-control-characters/null-character-ascii-code-0.html
+	//Nota: Apenas são considerados válidos, os caracteres pertencentes à tabela 'ASCII printable characters'
+	if(ch >= 32 && ch <= 126)
+		return 1;
+	return 0;
 }
 
 int IsValidUsername(char *username){
@@ -18,40 +20,129 @@ int IsValidUsername(char *username){
 	return 0;
 }
 
-void UpdateUsername(char *username, char* val) {
-	if(IsValidUsername(val))
+int UpdateUsername(char *username, char* val) {
+	if(IsValidUsername(val)){
 		strcpy(username, val);
+		return 1;
+	}
+	return 0;
 }
 
-void AskUsernameIfNull(char *username){
-	if(strcmp(username, "") == 0){
-		char usernameTmp[9];
+void AskUsernameIfEmpty(char *username){
+	if(strlen(username) == 0){
+		char usernameTmp[9] = {0};
 		do{
-			printf("Insira o seu username: ");
-			scanf("%8[^ \n]", usernameTmp);
-			FlushStdin();
-		}while(IsValidUsername(usernameTmp) == 0);
-		strcpy(username, usernameTmp);
+			printw("Insert your username: ");
+			scanw(" %8[^ \n]", usernameTmp);
+			clear();
+		}while(!UpdateUsername(username, usernameTmp));
 	}
 }
 
 void InitFromOpts(int argc, char* const argv[], char *username){
 	//Source: https://www.gnu.org/software/libc/manual/html_node/Getopt.html
 	int c;
-	while ((c = getopt(argc, argv, "n:")) != -1){
+	while ((c = getopt(argc, argv, "u:")) != -1){
 	    switch (c){
-	      case 'n': UpdateUsername(username, optarg); break;
+	      case 'u': UpdateUsername(username, optarg); break;
 	    }
 	}
 }
 
+void PrintLineNrs(int maxLinesTMP){
+	for(int i = 0; i < maxLinesTMP; i++)
+		mvprintw(i, 0, "%2d:", i);
+}
+
+void EnterLineEditMode(int posY, int maxColumns, int offset){
+	//Source #1: Exemplo NCURSES 1 -> Moodle
+	//Source #2: Exemplo NCURSES 2 -> Moodle
+	//Source #3: https://www.gnu.org/software/guile-ncurses/manual/html_node/Getting-characters-from-the-keyboard.html
+	//Source #4: https://invisible-island.net/ncurses/man/curs_getch.3x.html#h3-Keypad-mode
+	//Source #5: https://stackoverflow.com/questions/48274895/how-can-i-know-that-the-user-hit-the-esc-key-in-a-console-with-ncurses-linux?rq=1
+	//Source #6: http://invisible-island.net/ncurses/man/curs_getch.3x.html#h2-NOTES
+	/* 
+	 * NOTAS:
+	 * Nota #1: 27 é o valor decimal da tabela ASCII correspondente à tecla 'Escape'.
+	 * A biblioteca NCURSES define um timeout, que é usado ao premir esta tecla, para
+	 * poder distinguir se o utilizador clicou na tecla 'Escape' ou se usou uma
+	 * Input Escape Sequence. (VER: 'Source #4' e 'Source #5')
+	 *
+	 * Nota #2: Para evitar que o delay acima mencionado aconteça, é preciso
+	 * chamar a função notimeout(win, TRUE). (VER: 'Source #5')
+	 *
+	 * Nota #3: KEY_ENTER é o enter do 'num pad', 10 é o código ascii da
+	 * tecla 'Enter' do teclado. (VER: 'Source 6')
+	 *
+	 * Nota #4: No código desta função, não dou uso à função notimeout(...),
+	 * pelo que o delay referido ocorre antes do ciclo terminar. No entanto,
+	 * o delay não ocorre caso se insira uma Input Escape Sequence, que retorna como
+	 * primeiro carácter, o carácter cujo código ASCII é 27 (correspondente também
+	 * à tecla 'Escape' (VER: 'Nota #1')).
+	 *
+	 * Nota #5: A leitura da tecla 'Escape' é desencorajada, mas utilizada nesta aplicação,
+	 * para estar em conformidade com o enunciado do trabalho prático. (VER: 'Source #6')
+	 */
+
+	int posX = offset;
+	int ch;
+	maxColumns = maxColumns+offset;
+
+	cbreak();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	//Posiciona o cursor no inicio da linha
+	move(posY, posX);
+	refresh();
+
+	do{
+		ch = getch();
+		switch(ch){
+			case KEY_LEFT:{
+				if((posX-1)>=offset)
+					posX--;
+			}break;
+			case KEY_RIGHT:{
+				if((posX+1)<=maxColumns)
+					posX++;
+			}break;
+			case KEY_BACKSPACE:{
+				if((posX-1)>=offset){
+					posX--;
+					mvprintw(posY, posX, "%c", ' ');
+				}
+			}break;
+			default:{
+				if(IsValidChar(ch) && (posX+1)<=maxColumns){
+					mvprintw(posY, posX, "%c", ch);
+					posX++;
+				}
+			}break;
+		}	
+		move(posY, posX);
+		refresh();
+	}while(ch != 27);
+}
+
+void InitTextEditor(char *username, CommonSettings commonSettings){
+	AskUsernameIfEmpty(username);
+	PrintLineNrs(commonSettings.maxLines);
+	EnterLineEditMode(0, commonSettings.maxColumns, 3);
+}
+
 int main(int argc, char* const argv[]){
-	char username[9];
-	CommonSettings settings;
+	//A struct será preenchida com a informação do servidor, enviada por namepipe. (numa meta futura)
+	CommonSettings commonSettings;
+	commonSettings.maxLines = 15;
+	commonSettings.maxColumns = 45;
 
+	char username[9] = {0};
+	//---------------------------------------
+	initscr();
+	clear();
 	InitFromOpts(argc, argv, username);
-	AskUsernameIfNull(username);
-
-	printf("USERNAME: %s\n", username);
+	InitTextEditor(username, commonSettings);
+	endwin();
 	return 0;
 }
